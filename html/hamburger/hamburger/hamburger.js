2f2b831df69e65426c255f47c924d14b9bbf879b
nxxxtyetdecided <86827063+nxxxtyetdecided@users.noreply.github.com>
2022-01-22 22:32:41 +0900

    Delete html/hamburger directory

--- html/hamburger/hamburger/hamburger.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('btn')
-const nav = document.getElementById('nav')
-
-btn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-    btn.classList.toggle("active");
-});
